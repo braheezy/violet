b70a9f42f1a1ceb1063aa2281b8f99a42dfd2246
braheezy <michael.c.braha@gmail.com>
2022-12-24 22:03:03 -0800

    Delete tests

---
+++ internal/app/app_test.go
@@ -0,0 +1,14 @@
+package app
+
+/*
+Test Goals:
+   1. Model: Test the data structures and their properties, such as the Environment and VM structs in the Violet model.
+
+   2. View: Test the output of the View method to ensure that it is correct for different input data.
+
+   3. Update: Test the Update method to ensure that it updates the model correctly in response to different messages.
+
+   4. Interactions with the VagrantClient: Test the interactions with the VagrantClient, such as the RunCommand function, to ensure that they are working correctly and returning the expected results.
+
+   5. User input handling: Test the handling of user input, such as keyboard and mouse events, to ensure that they are correctly processed and result in the expected changes to the model.
+*/
