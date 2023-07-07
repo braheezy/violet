a5f255cf86cdc96fc05501492c09e31254b47007
braheezy <michael.c.braha@gmail.com>
2023-07-06 18:47:11 -0700

    chore: Add comments and improve code readability of parse function

--- pkg/vagrant/vagrant.go
+++ pkg/vagrant/vagrant.go
@@ -132,6 +132,7 @@ type MachineInfo struct {
 	Fields map[string]string
 }
 
+// Returns true if item is in slice.
 func Contains(slice []MachineInfo, item MachineInfo) bool {
 	for _, s := range slice {
 		if len(s.MachineID) > 0 && s.MachineID == item.MachineID {
@@ -147,6 +148,20 @@ func Contains(slice []MachineInfo, item MachineInfo) bool {
 // Generically parses the output from a Vagrant command and returns the result.
 // Multi-machine environments may result in a list of Result objects
 func ParseVagrantOutput(output string) []MachineInfo {
+	/*
+		This function operators on the output of --machine-readable Vagrant commands: https://developer.hashicorp.com/vagrant/docs/cli/machine-readable.
+
+		The format is:
+			timestamp,target,type,data...
+
+			timestamp is a Unix timestamp in UTC of when the message was printed.
+
+			target is the target of the following output. This is empty if the message is related to Vagrant globally. Otherwise, this is generally a machine name so you can relate output to a specific machine when multi-VM is in use.
+
+			type is the type of machine-readable message being outputted. There are a set of standard types which are covered later.
+
+			data is zero or more comma-separated values associated with the prior type. The exact amount and meaning of this data is type-dependent, so you must read the documentation associated with the type to understand fully.
+	*/
 	// Compile regular expressions for each field.
 	fields := make(map[string]*regexp.Regexp)
 	// DEV: Add more fields here as needed.
@@ -162,29 +177,37 @@ func ParseVagrantOutput(output string) []MachineInfo {
 		matched := false
 		for field, re := range fields {
 			if m := re.FindStringSubmatch(line); m != nil {
+				target := m[1]
+				// This loops manages findings for machines we might have already seen.
 				for _, r := range results {
-					if r.Name == m[1] && r.Name != "" {
+					// When the name matches (and isn't empty), it's a machine seen before.
+					if r.Name == target && r.Name != "" {
+						// We're about to switch the result to a different machine so make sure
+						// it's in the results list before we "drop" it.
 						if !Contains(results, result) {
 							results = append(results, result)
 						}
+						// Update this machine instead of whatever we we're tracking previously.
 						result = r
 					}
 				}
+				// Now, fill in machine data, keeping an eye out for new Names or MachineIDs, the clear indication a new machine has been found.
 				// metadata lines de-lineate VMs and are a good place to grab the name.
 				if field == "metadata" {
 					// Save name if it's the first we've seen
 					if result.Name == "" {
-						result.Name = m[1]
-					} else if result.Name != m[1] {
+						// NB: target might be "" too, that's okay.
+						result.Name = target
+					} else if result.Name != target {
 						// New VM found. Create new Result
 						results = append(results, result)
-						result = MachineInfo{Name: m[1], Fields: make(map[string]string)}
+						result = MachineInfo{Name: target, Fields: make(map[string]string)}
 					}
 				} else if field == "machine-id" {
 					if result.MachineID != "" {
 						// New VM found. Create new Result
 						results = append(results, result)
-						result = MachineInfo{Name: m[1], MachineID: m[2], Fields: make(map[string]string)}
+						result = MachineInfo{Name: target, MachineID: m[2], Fields: make(map[string]string)}
 					} else {
 						result.MachineID = m[2]
 					}
